13947a10abc875765a69960ef101145c04cfa140
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-16 23:19:37 +0200

    Talking.h

--- Hw-28.2/TalkingBook.h
+++ Hw-28.2/TalkingBook.h
@@ -1,5 +1,15 @@
 #pragma once
-class TalkingBook
-{
-};
+#include "Book.h"
+#include "VoiceRecording.h"
 
+class TalkingBook : public Book, public VoiceRecording {
+private:
+    int tapes;
+
+public:
+    TalkingBook(string a, string e, string pubDate, long isbn,
+        string s, int d, string recDate,
+        int t);
+
+    void showAll() const;
+};
