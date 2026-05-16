d723db9b4fcc63fccd0386521283930456c5b56a
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-16 23:15:23 +0200

    Voice.h

--- Hw-28.2/VoiceRecording.h
+++ Hw-28.2/VoiceRecording.h
@@ -1,5 +1,15 @@
 #pragma once
-class VoiceRecording
-{
-};
+#include <string>
+using namespace std;
 
+class VoiceRecording {
+protected:
+    string speaker;
+    int duration;
+    string recordingDate;
+
+public:
+    VoiceRecording(string s, int d, string date);
+
+    void showVoice() const;
+};
