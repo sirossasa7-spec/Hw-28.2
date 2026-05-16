dd9777ab99762f742430378989fe5876e9ead0ae
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-16 23:27:31 +0200

    main

--- Hw-28.2/Hw-28.2.cpp
+++ Hw-28.2/Hw-28.2.cpp
@@ -1,1 +1,19 @@
-﻿
+﻿#include "TalkingBook.h"
+#include <iostream>
+
+int main() {
+    Book b("George Orwell", "1st", "1949", 123456);
+    VoiceRecording v("John Smith", 120, "2020");
+    TalkingBook tb("J.K. Rowling", "2nd", "2001", 987654,
+        "Emma Watson", 300, "2022", 5);
+
+    b.showBook();
+    cout << endl;
+
+    v.showVoice();
+    cout << endl;
+
+    tb.showAll();
+
+    return 0;
+}
