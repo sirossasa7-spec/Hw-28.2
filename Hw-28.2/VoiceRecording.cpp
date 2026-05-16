d3d34f636dfc57d0e4bff47cd2efde27cb9157e7
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-16 23:22:40 +0200

    Voice.cpp

--- Hw-28.2/VoiceRecording.cpp
+++ Hw-28.2/VoiceRecording.cpp
@@ -1,9 +1,15 @@
 #include "VoiceRecording.h"
+#include <iostream>
 
-VoiceRecording::VoiceRecording(string s, int d, string date)
-{
+VoiceRecording::VoiceRecording(string s, int d, string date) {
+    speaker = s;
+    duration = d;
+    recordingDate = date;
 }
 
-void VoiceRecording::showVoice() const
-{
+void VoiceRecording::showVoice() const {
+    cout << "=== Voice Recording ===\n";
+    cout << "Speaker: " << speaker << endl;
+    cout << "Duration: " << duration << " min" << endl;
+    cout << "Recording date: " << recordingDate << endl;
 }
