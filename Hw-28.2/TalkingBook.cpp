9f1593585c0fb956c84c10ba7f0e8bfe4a34d3e8
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-16 23:25:03 +0200

    Talking.cpp

--- Hw-28.2/TalkingBook.cpp
+++ Hw-28.2/TalkingBook.cpp
@@ -1,9 +1,17 @@
 #include "TalkingBook.h"
+#include <iostream>
 
-TalkingBook::TalkingBook(string a, string e, string pubDate, long isbn, string s, int d, string recDate, int t)
-{
+TalkingBook::TalkingBook(string a, string e, string pubDate, long isbn,
+    string s, int d, string recDate,
+    int t)
+    : Book(a, e, pubDate, isbn),
+    VoiceRecording(s, d, recDate) {
+    tapes = t;
 }
 
-void TalkingBook::showAll() const
-{
+void TalkingBook::showAll() const {
+    cout << "\n=== Talking Book ===\n";
+    showBook();
+    showVoice();
+    cout << "Tapes: " << tapes << endl;
 }
